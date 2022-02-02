badf411aa43c7a5fedb63a37f64d72e3f7e51e46
BrendoEnomoto <50683923+BrendoEnomoto@users.noreply.github.com>
2022-02-02 03:06:41 -0300

    adiciona a base na lua

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ starMesh.layers.set(1);
 scene.add(starMesh);
 
 //earth geometry
-const earthgeometry = new THREE.SphereGeometry(9.8, 64, 64);
+const earthgeometry = new THREE.SphereGeometry(19.6, 64, 64);
 
 //earth material
 const earthMaterial = new THREE.MeshPhongMaterial({
@@ -91,7 +91,7 @@ scene.add(earthMesh);
 
 
 //cloud geometry
-const cloudgeometry = new THREE.SphereGeometry(10, 64, 64);
+const cloudgeometry = new THREE.SphereGeometry(19.8, 64, 64);
 
 //cloud material
 const cloudMaterial = new THREE.MeshPhongMaterial({
@@ -105,7 +105,7 @@ earthMesh.layers.set(0);
 scene.add(cloud);
 
 //moon geometry
-const moongeometry = new THREE.SphereGeometry(1, 64, 64);
+const moongeometry = new THREE.SphereGeometry(2, 64, 64);
 
 //moon material
 const moonMaterial = new THREE.MeshPhongMaterial({
@@ -120,42 +120,40 @@ const moonMaterial = new THREE.MeshPhongMaterial({
 const moonMesh = new THREE.Mesh(moongeometry, moonMaterial);
 moonMesh.receiveShadow = true;
 moonMesh.castShadow = true;
-moonMesh.position.x = 20;
+moonMesh.position.x = 40;
 moonMesh.layers.set(0);
-scene.add(moonMesh);
+
+//adiciona os modelos 3d
+var loader = new GLTFLoader();
+//rocket launcher
+var launcher;
+
+loader.load( "./models/launcher/scene.glb",function(gltf){gltf.scene.traverse(function(node){
+    if ( node.isMesh ) { 
+        node.castShadow = true;
+        node.receiveShadow = true;
+    }
+});
+launcher = gltf.scene;
+launcher.position.set(0,40)
+/*
+launcher.position.set(37.995,0,0);
+launcher.rotation.z = 1.5;
+launcher.scale.set(0.005,0.005,0.005); */
+
+
+scene.add(launcher);
+} );
+
 
 var moonPivot = new THREE.Object3D();
 earthMesh.add(moonPivot);
 moonPivot.add(moonMesh);
 
-var cameraPivot = new THREE.Object3D();
-moonMesh.add(cameraPivot);
-cameraPivot.add(camera);
-/* 
-//importar os modelos 3d
-const loader = new GLTFLoader();
-
-var launcher;
-
-loader.load("./models/launcher/scene.gltf", function(gltf){
-    launcher = gltf.scene;
-    launcher.scale.set(0.01,0.01,0.01)
-    launcher.position.set(0,3,0)
-    scene.add(gltf.scene);
-});
-
-var rocket;
-
-loader.load("./models/rocket/scene.gltf", function(gltf){
-    rocket = gltf.scene;
-    rocket.position.set(0,3,0)
-    rocket.scale.set(0.01,0.01,0.01)
-    scene.add(gltf.scene);
-}); */
 
 //ambient light
-const ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientlight);
+//const ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+//scene.add(ambientlight);
 
 //var light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
 //scene.add(light);
@@ -182,6 +180,57 @@ pointLight.shadowMapHeight = 2048;
 pointLight.position.set(50, 20, 60);
 scene.add(pointLight2);
 
+
+// launcher lights
+
+var spot_light_1 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_1.castShadow = true;
+spot_light_1.target.position.set(0,43,0);
+spot_light_1.position.set(13.5,50,25);
+scene.add(spot_light_1);
+scene.add(spot_light_1.target);
+spot_light_1.shadow.camera.zoom = 1;
+
+var spot_light_2 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_2.castShadow = true;
+spot_light_2.target.position.set(0,43,0);
+spot_light_2.position.set(13.5,47,25);
+scene.add(spot_light_2);
+scene.add(spot_light_2.target);
+spot_light_2.shadow.camera.zoom = 1;
+
+var spot_light_3 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_3.castShadow = true;
+spot_light_3.target.position.set(0,43,0);
+spot_light_3.position.set(16.5,48,-14);
+scene.add(spot_light_3);
+scene.add(spot_light_3.target);
+spot_light_3.shadow.camera.zoom = 1;
+
+var spot_light_4 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_4.castShadow = true;
+spot_light_4.target.position.set(0,43,0);
+spot_light_4.position.set(-11,47,-14);
+scene.add(spot_light_4);
+scene.add(spot_light_4.target);
+spot_light_4.shadow.camera.zoom = 1;
+
+var spot_light_5 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_5.castShadow = true;
+spot_light_5.target.position.set(0,43,0);
+spot_light_5.position.set(-11,50,-14);
+scene.add(spot_light_5);
+scene.add(spot_light_5.target);
+spot_light_5.shadow.camera.zoom = 1;
+
+var spot_light_6 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
+spot_light_6.castShadow = true;
+spot_light_6.target.position.set(0,43,0);
+spot_light_6.position.set(-18,48,15);
+scene.add(spot_light_6);
+scene.add(spot_light_6.target);
+spot_light_6.shadow.camera.zoom = 1;
+
 //resize listner
 window.addEventListener(
     "resize",
@@ -194,15 +243,33 @@ window.addEventListener(
     false
   );
 
-camera.position.set(0,0,10)
+camera.position.set(0,4,50)
+
+var launcherPivot = new THREE.Object3D();
+var cameraPivot = new THREE.Object3D()
 
 const animate = () => {
+    if (launcher) {
+        //moon pivot
+        moonPivot.rotation.y -= 0.005;
+        moonPivot.rotation.x = 0.5;
+
+        //camera pivot
+        //launcher.add(cameraPivot);
+        //cameraPivot.add(camera);
+        
+        //launcher pivot
+        //earthMesh.add(launcherPivot);
+        //launcherPivot.add(launcher);
+        //launcherPivot.rotation.y -= 0.005;
+        //launcherPivot.rotation.x = 0.5;
+    }
     requestAnimationFrame(animate);
     cloud.rotation.y-=0.0002;
     //moon rotation
-    moonPivot.rotation.y -= 0.005;
-    moonPivot.rotation.x = 0.5;
-    cameraPivot.rotation.y += 0.001;
+
+    //camerapivot
+    //cameraPivot.rotation.y += 0.001;
     starMesh.rotation.y += 0.0002;
     camera.layers.set(1);
     bloomComposer.render();
