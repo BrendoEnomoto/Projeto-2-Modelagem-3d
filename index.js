44177280c2621d8509cc8246bc29f81877eaafb7
BrendoEnomoto <50683923+BrendoEnomoto@users.noreply.github.com>
2022-02-02 13:49:26 -0300

    conserta alguns bugs e coloca o launcher na lua

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { UnrealBloomPass } from "./UnrealBloomPass.js";
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
 
-//renderer
+//renderização
 var renderer = new THREE.WebGLRenderer({
     antialias: true,
 });
@@ -18,7 +18,14 @@ renderer.setClearColor(0x000000, 0.0);
 document.body.appendChild(renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//bloom renderer
+//opções de sombremento
+renderer.shadowMap.enabled = true;
+//renderer.shadowMapType = THREE.PCFSoftShadowMap;
+
+
+
+
+//renderização bloom 
 const renderScene = new RenderPass(scene, camera);
 const bloomPass = new UnrealBloomPass(
   new THREE.Vector2(window.innerWidth, window.innerHeight),
@@ -27,7 +34,7 @@ const bloomPass = new UnrealBloomPass(
   0.85
 );
 bloomPass.threshold = 0;
-bloomPass.strength = 2; //intensity of glow
+bloomPass.strength = 2;
 bloomPass.radius = 0;
 const bloomComposer = new EffectComposer(renderer);
 bloomComposer.setSize(window.innerWidth, window.innerHeight);
@@ -35,9 +42,12 @@ bloomComposer.renderToScreen = true;
 bloomComposer.addPass(renderScene);
 bloomComposer.addPass(bloomPass);
 
+//Orbitcontrols
 const controlador = new OrbitControls(camera, renderer.domElement);
 
-//red sun object
+
+//Sol azul
+
 const color = new THREE.Color("#B22222");
 const geometry = new THREE.IcosahedronGeometry(1, 15);
 const material = new THREE.MeshBasicMaterial({ color: color });
@@ -46,7 +56,7 @@ sphere.position.set(-50, 20, -60);
 sphere.layers.set(1);
 scene.add(sphere);
 
-//blue sun object
+//Sol vermelho
 const color2 = new THREE.Color("#4169E1");
 const geometry2 = new THREE.IcosahedronGeometry(1, 15);
 const material2 = new THREE.MeshBasicMaterial({ color: color2 });
@@ -55,25 +65,25 @@ sphere2.position.set(50, 20, 60);
 sphere2.layers.set(1);
 scene.add(sphere2);
 
-// galaxy geometry
+// geometria da galaxia
 const starGeometry = new THREE.SphereGeometry(80, 64, 64);
 
-// galaxy material
+// material da galaxia
 const starMaterial = new THREE.MeshBasicMaterial({
   map: THREE.ImageUtils.loadTexture("texture/galaxy1.png"),
   side: THREE.BackSide,
   transparent: true,
 });
 
-// galaxy mesh
+// galaxia mesh
 const starMesh = new THREE.Mesh(starGeometry, starMaterial);
 starMesh.layers.set(1);
 scene.add(starMesh);
 
-//earth geometry
+//geometria da terra
 const earthgeometry = new THREE.SphereGeometry(19.6, 64, 64);
 
-//earth material
+//material da terra
 const earthMaterial = new THREE.MeshPhongMaterial({
   roughness: 1,
   metalness: 0,
@@ -82,7 +92,7 @@ const earthMaterial = new THREE.MeshPhongMaterial({
   bumpScale: 0.3,
 });
 
-//earthMesh
+//mash da terra
 const earthMesh = new THREE.Mesh(earthgeometry, earthMaterial);
 earthMesh.receiveShadow = true;
 earthMesh.castShadow = true;
@@ -90,24 +100,24 @@ earthMesh.layers.set(0);
 scene.add(earthMesh);
 
 
-//cloud geometry
+//geometria das nuvens
 const cloudgeometry = new THREE.SphereGeometry(19.8, 64, 64);
 
-//cloud material
+//material das nuvens
 const cloudMaterial = new THREE.MeshPhongMaterial({
   map: THREE.ImageUtils.loadTexture("texture/earthCloud.png"),
   transparent: true,
 });
 
-//cloudMesh
+//nuvem mesh
 const cloud = new THREE.Mesh(cloudgeometry, cloudMaterial);
 earthMesh.layers.set(0);
 scene.add(cloud);
 
-//moon geometry
+//geometria da nuvem
 const moongeometry = new THREE.SphereGeometry(2, 64, 64);
 
-//moon material
+//material da nuvem 
 const moonMaterial = new THREE.MeshPhongMaterial({
   roughness: 5,
   metalness: 0,
@@ -116,14 +126,14 @@ const moonMaterial = new THREE.MeshPhongMaterial({
   bumpScale: 0.02,
 });
 
-//moonMesh
+//mash da lua
 const moonMesh = new THREE.Mesh(moongeometry, moonMaterial);
 moonMesh.receiveShadow = true;
 moonMesh.castShadow = true;
 moonMesh.position.x = 40;
 moonMesh.layers.set(0);
 
-//moon rotation pivot
+//pivo de rotação da lua
 var moonPivot = new THREE.Object3D();
 earthMesh.add(moonPivot);
 moonPivot.add(moonMesh);
@@ -131,7 +141,7 @@ moonPivot.add(moonMesh);
 //adiciona os modelos 3d
 var loader = new GLTFLoader();
 
-//rocket launcher
+//base de lançamento
 var launcher;
 
 loader.load( "./models/launcher/scene.glb",function(gltf){gltf.scene.traverse(function(node){
@@ -141,13 +151,9 @@ loader.load( "./models/launcher/scene.glb",function(gltf){gltf.scene.traverse(fu
     }
 });
 launcher = gltf.scene;
-launcher.position.set(0,40)
-/*
 launcher.position.set(37.995,0,0);
 launcher.rotation.z = 1.5;
-launcher.scale.set(0.005,0.005,0.005); */
-
-
+launcher.scale.set(0.005,0.005,0.005);
 scene.add(launcher);
 } );
 
@@ -158,28 +164,19 @@ var ISS;
 loader.load( "./models/iss/scene.glb",function(gltf){gltf.scene.traverse(function(node){
   if ( node.isMesh ) { 
       node.castShadow = true;
-      node.receiveShadow = true;
+      node.receiveShadow = false;
   }
 });
 ISS = gltf.scene;
 ISS.scale.set(0.5,0.5,0.5);
-ISS.position.set(20,0)
+ISS.position.set(20,0);
+ISS.layers.set(0)
 scene.add(ISS);
 } );
 
 
-
-
-
-//ambient light
-//const ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
-//scene.add(ambientlight);
-
-//var light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
-//scene.add(light);
-
-//red sun point Light 
-const pointLight = new THREE.PointLight(0x4169E1, 2);
+//sol vermelho ponto de luz 
+const pointLight = new THREE.PointLight(0xB22222, 4);
 pointLight.castShadow = true;
 pointLight.shadowCameraVisible = true;
 pointLight.shadowBias = 0.00001;
@@ -189,69 +186,19 @@ pointLight.shadowMapHeight = 2048;
 pointLight.position.set(-50, 20, -60);
 scene.add(pointLight);
 
-//blue sun point Light 
-const pointLight2 = new THREE.PointLight(0xB22222, 2);
-pointLight.castShadow = true;
-pointLight.shadowCameraVisible = true;
-pointLight.shadowBias = 0.00001;
-pointLight.shadowDarkness = 0.2;
-pointLight.shadowMapWidth = 2048;
-pointLight.shadowMapHeight = 2048;
-pointLight.position.set(50, 20, 60);
+
+//sol azul ponto de luz 
+const pointLight2 = new THREE.PointLight(0x4169E1, 4);
+pointLight2.castShadow = true;
+pointLight2.shadowCameraVisible = true;
+pointLight2.shadowBias = 0.00001;
+pointLight2.shadowDarkness = 0.2;
+pointLight2.shadowMapWidth = 2048;
+pointLight2.shadowMapHeight = 2048;
+pointLight2.position.set(50, 20, 60);
 scene.add(pointLight2);
 
-
-// launcher lights
-
-var spot_light_1 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_1.castShadow = true;
-spot_light_1.target.position.set(0,43,0);
-spot_light_1.position.set(13.5,50,25);
-scene.add(spot_light_1);
-scene.add(spot_light_1.target);
-spot_light_1.shadow.camera.zoom = 1;
-
-var spot_light_2 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_2.castShadow = true;
-spot_light_2.target.position.set(0,43,0);
-spot_light_2.position.set(13.5,47,25);
-scene.add(spot_light_2);
-scene.add(spot_light_2.target);
-spot_light_2.shadow.camera.zoom = 1;
-
-var spot_light_3 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_3.castShadow = true;
-spot_light_3.target.position.set(0,43,0);
-spot_light_3.position.set(16.5,48,-14);
-scene.add(spot_light_3);
-scene.add(spot_light_3.target);
-spot_light_3.shadow.camera.zoom = 1;
-
-var spot_light_4 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_4.castShadow = true;
-spot_light_4.target.position.set(0,43,0);
-spot_light_4.position.set(-11,47,-14);
-scene.add(spot_light_4);
-scene.add(spot_light_4.target);
-spot_light_4.shadow.camera.zoom = 1;
-
-var spot_light_5 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_5.castShadow = true;
-spot_light_5.target.position.set(0,43,0);
-spot_light_5.position.set(-11,50,-14);
-scene.add(spot_light_5);
-scene.add(spot_light_5.target);
-spot_light_5.shadow.camera.zoom = 1;
-
-var spot_light_6 = new THREE.SpotLight(new THREE.Color(0xffffff),3);
-spot_light_6.castShadow = true;
-spot_light_6.target.position.set(0,43,0);
-spot_light_6.position.set(-18,48,15);
-scene.add(spot_light_6);
-scene.add(spot_light_6.target);
-spot_light_6.shadow.camera.zoom = 1;
-
-//resize listner
+//adciona a detecção automatica do tamamanho da janela de exibição 
 window.addEventListener(
     "resize",
     () => {
@@ -271,33 +218,33 @@ var issPivot = new THREE.Object3D();
 
 const animate = () => {
     if (launcher) {
-        //moon rotation
+        //rotação da lua
         moonPivot.rotation.y -= 0.005;
         moonPivot.rotation.x = 0.5;
 
-        //camera pivot
+        //pivo de rotação da base
+        earthMesh.add(launcherPivot);
+        launcherPivot.add(launcher);
+        launcherPivot.rotation.y -= 0.005;
+        launcherPivot.rotation.x = 0.5;
+
+        //pivo da camera
         //launcher.add(cameraPivot);
         //cameraPivot.add(camera);
-        
-        //launcher pivot
-        //earthMesh.add(launcherPivot);
-        //launcherPivot.add(launcher);
-        //launcherPivot.rotation.y -= 0.005;
-        //launcherPivot.rotation.x = 0.5;
     }
     if (ISS) {
-        //ISS pivot
+        //pivo de rotação da ISS 
         earthMesh.add(issPivot);
         issPivot.add(ISS);
         issPivot.rotation.y -= 0.002;
         issPivot.rotation.x = 0.005;
-        //camera pivot
-        ISS.add(cameraPivot);
-        cameraPivot.add(camera);
+        //pivo da camera
+        //ISS.add(cameraPivot);
+        //cameraPivot.add(camera);
   }
     requestAnimationFrame(animate);
     cloud.rotation.y-=0.0002;
-    //camerapivot
+    //pivo da camera
     //cameraPivot.rotation.y += 0.001;
     starMesh.rotation.y += 0.0002;
     camera.layers.set(1);
@@ -308,13 +255,14 @@ const animate = () => {
   };
 animate();
 
+// adiciona musica de fundo
 /* const listener = new THREE.AudioListener();
 camera.add( listener );
 const sound = new THREE.Audio( listener );
 const audioLoader = new THREE.AudioLoader();
-audioLoader.load( './projeto 2 audio teste.wav', function( buffer ) {
+audioLoader.load( './interstellar.mp3', function( buffer ) {
 	sound.setBuffer( buffer );
 	sound.setLoop( true );
 	sound.setVolume( 0.3 );
 	sound.play();
-}); */
+});  */
